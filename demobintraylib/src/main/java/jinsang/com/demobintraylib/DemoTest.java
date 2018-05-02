87a34def68ea146022738fcd0067fd2b22cc2718
jinsang <jinsang@nhnent.com>
2018-05-02 18:51:04 +0900

    bintray 설정

---
+++ demobintraylib/src/main/java/jinsang/com/demobintraylib/DemoTest.java
@@ -0,0 +1,11 @@
+package jinsang.com.demobintraylib;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.Log;
+
+public class DemoTest {
+    public void showToast(Context context, Resources res) {
+        Log.d("DemoTest::showToast", "쿨럭쿨럭");
+    }
+}
